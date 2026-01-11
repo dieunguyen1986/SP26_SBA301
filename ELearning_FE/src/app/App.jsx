8958a5950f6b5376ebe32f335727284fd54337f7
Dieu Nguyen <dieunguyencva@gmail.com>
2026-01-12 00:21:39 +0700

    Add ELearning_FE project alongside existing project

---
+++ ELearning_FE/src/app/App.jsx
@@ -0,0 +1,15 @@
+import MentorSection from "../features/public-site/components/mentor/MentorSection";
+import HeroSection from "@/features/public-site/components/HeroSection"
+import "./App.css";
+
+function App() {
+  return (
+    <>
+    <HeroSection/>
+      <MentorSection />
+
+    </>
+  );
+}
+
+export default App;
